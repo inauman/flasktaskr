f784248a51bcf33c92b97299850795c3c0ea2657
Mohammad Nauman <md.nauman@gmail.com>
2020-06-20 03:53:34 -0700

    resolved import issue

--- project/run.py
+++ project/run.py
@@ -1,3 +1,7 @@
-from views import app
+#below two lines are for importing a package from parent or sibling.
+import sys
+sys.path.insert(0, '..')
+
+from project.views import app
 
 app.run(debug=True)
