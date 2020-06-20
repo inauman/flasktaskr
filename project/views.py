90beda3426b3ead1431f343fcedee2d2dd804eed
Mohammad Nauman <md.nauman@gmail.com>
2020-06-20 03:09:41 -0700

    Added router handlers for login and logout

--- project/views.py
+++ project/views.py
@@ -20,3 +20,23 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap
 
+# route handlers
+
+@app.route('/logout/')
+def logout():
+    session.pop('logged_in', None)
+    flash('Goodbye')
+    return redirect(url_for('login'))
+
+@app.route('/', method=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] \
+            or request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid Credentials. Please try again."
+            return render_template('login.html', error=error)
+        else:
+            session['logged_in'] = True
+            flash('Welcome')
+            return redirect(url_for('tasks'))
+    return render_template('login.html')
