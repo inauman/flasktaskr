30580307eabae03f58b9f0199cb814dfd47d74e3
Mohammad Nauman <md.nauman@gmail.com>
2020-06-20 01:24:20 -0700

    Created the config settings for db, cross-site scripting, and secret key

--- _config.py
+++ _config.py
@@ -5,7 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
+
+# setting to enable protection against cross-site scripting.
 WTF_CSRF_ENABLED = True
+
+# this setting is used for enabling creation of cryptographic
+# token to validate form. Used in conjunction w/ WTF_CSRF
 SECRET_KEY = 'myprecious'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
