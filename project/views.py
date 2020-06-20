28f765ed5f364c3acc8469f6a21e5033c2f93e83
Mohammad Nauman <md.nauman@gmail.com>
2020-06-20 12:35:12 -0700

    Added new route for tasks

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 import sqlite3
 from functools import wraps
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, g, redirect, render_template, request, session, url_for
 
 # config
 app = Flask(__name__)
@@ -41,3 +41,35 @@ def login():
             flash('Welcome')
             return redirect(url_for('tasks'))
     return render_template('login.html')
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+
+    g.db = connect_db()
+    cursor = g.db.execute('select name, due_date, priorirty, task_id from tasks \
+        where status=1')
+
+    # let's use list comprehension with dict to populate the 
+    # data from db query
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) \
+            for row in cursor.fetchall()
+    ]
+
+    cursor = g.db.execute('select name, due_date, priority, task_id from tasks \
+        where status=0')
+
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) \
+            for row in cursor.fetchall()
+    ]
+
+    g.db.close()
+
+    return render_template('tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
+    
