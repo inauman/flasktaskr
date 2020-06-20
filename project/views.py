0ba2b569d0ec10c5081c0cdbfd5dfb2549bfe868
Mohammad Nauman <md.nauman@gmail.com>
2020-06-20 13:27:54 -0700

    Created HTML for tasks

--- project/views.py
+++ project/views.py
@@ -22,7 +22,7 @@ def login_required(test):
     return wrap
 
 # route handlers
-
+# logout
 @app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
@@ -42,6 +42,7 @@ def login():
             return redirect(url_for('tasks'))
     return render_template('login.html')
 
+# list tasks
 @app.route('/tasks/')
 @login_required
 def tasks():
@@ -73,3 +74,55 @@ def tasks():
         closed_tasks=closed_tasks
     )
     
+# add new task
+@app.route('/add/', methods=['POST'])
+@login_required
+def new_task():
+    g.db = connect_db()
+    name = request.form['name']
+    date = request.form['due_date']
+    priority = request.form['priority']
+
+    if not name or not date or not priority:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('tasks'))
+    else:
+        g.db.execute('INSERT INTO tasks(name, due_date, priority, status) \
+            VALUES(?, ?, ?, 1)', name, date, priority)
+
+        g.db.commit()
+        g.db.close()
+        flash("New entry was successfully posted. Thanks.")
+        return redirect(url_for('tasks'))
+
+# Mark task complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.db.execute(
+        f'UPDATE tasks set status=0 WHERE task_id={ str(task_id) }'
+    )  
+
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as complete')
+    return redirect(url_for('tasks'))
+
+
+# delete tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    print(type(g.db)) 
+    g.db.execute(
+        f'DELETE FROM tasks WHERE task_id={ str(task_id) }'
+    )
+    g.db.commit()
+    g.db.close()
+    flash('The task was deleted')
+    return redirect(url_for('tasks'))
+
+
+
