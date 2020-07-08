aa1dd72807cb219a40b124f4f6317d89cbbded31
Mohammad Nauman <nauman@Mohammads-iMac.local>
2020-07-07 17:32:45 -0700

    Hello

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,11 @@
 from fabric import Connection, task
 
 with Connection('localhost') as ctx:
-    # c.local("echo 'Hi there'")
-    # #c.local("exec ~/.zshrc")
-    # c.local("nosetests -v")
-    # message = input("Enter a git commit message: ")
-    # c.local(f"git add . && git commit -am '{message}'")
-    # c.local("git push origin master")
 
     @task
     def test(ctx):
-        ctx.run("nosetests -v")
+        ctx.run("source .env")
+        #ctx.run("nosetests -v")
     @task
     def commit(ctx):
         message = input("Enter a git commit message: ")
